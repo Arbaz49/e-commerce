35b33f3ddf389bec26d9567aa311b0874dbc1b92
Arbaz <arbaz24.3.20@gmail.com>
2024-03-14 15:55:07 +0530

    cart drawer ui updated

--- src/components/CartDrawer.jsx
+++ src/components/CartDrawer.jsx
@@ -6,13 +6,13 @@ import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import { Badge } from "@mui/material";
 import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
-import {clearCart,addQuantity,removeQuantity} from "../store/CartSlice"
+import { clearCart, addQuantity, removeQuantity } from "../store/CartSlice"
 import { useRouter } from "next/navigation";
 export default function CartDrawer() {
   const cartLength = useSelector((state) => state.cart.cartArray.length)
   const cartArray = useSelector((state) => state.cart.cartArray)
 
-  console.log("length",cartArray)
+  console.log("length", cartArray)
   const [state, setState] = React.useState({
     right: false,
   });
@@ -26,30 +26,30 @@ export default function CartDrawer() {
   const calculateTotal = () => {
     return cartArray.reduce((total, product) => total + product.price * product.quantity, 0);
   };
-  
-  React.useEffect(()=>{
 
-  },[])
+  React.useEffect(() => {
+
+  }, [])
   const toggleDrawer = (anchor, open) => (event) => {
     if (
-        event.type === "keydown" &&
-        (event.key === "Tab" || event.key === "Shift")
-      ) {
-        return;
-      }
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
     setState({ ...state, [anchor]: open });
   };
-  const dispatch=useDispatch()
-  const handleClick = () => {};
+  const dispatch = useDispatch()
+  const handleClick = () => { };
   const handleClear = () => {
     setState({ ...state, ["right"]: false });
     dispatch(clearCart())
   }
-  const router=useRouter()
-  const handleLogOut=()=>{
-localStorage.removeItem("User")
-setState({ ...state, ["right"]: false });
-router.push("/")
+  const router = useRouter()
+  const handleLogOut = () => {
+    localStorage.removeItem("User")
+    setState({ ...state, ["right"]: false });
+    router.push("/")
   }
 
   return (
@@ -63,7 +63,7 @@ router.push("/")
               color="secondary"
               style={{ color: "pink" }}
             >
-              <ShoppingCartOutlinedIcon style={{color:"rgb(236 69 153)",zIndex:1}}  />
+              <ShoppingCartOutlinedIcon style={{ color: "rgb(236 69 153)", zIndex: 1 }} />
             </Badge>
           </Button>
 
@@ -71,44 +71,47 @@ router.push("/")
 
 
           <Drawer
-          style={{minWidth:600}}
+            style={{ minWidth: 600 }}
             anchor={anchor}
             open={state[anchor]}
             onClose={toggleDrawer(anchor, false)}
           >
-            <div style={{minWidth:"500px"}} className="bg-pink-400 h-full">
-            <h2 className="text-center font-bold text-3xl">Cart</h2>
+            <div style={{ minWidth: "500px" }} className="bg-pink-400 h-full">
+              <h2 className="text-center font-bold text-3xl">Cart</h2>
               <img
                 width={"50px"}
                 height={"50px"}
-                style={{borderRadius:"50%"}}
+                style={{ borderRadius: "50%" }}
                 className="m-auto"
                 src="https://rukminim1.flixcart.com/image/612/612/xif0q/t-shirt/9/1/o/m-db1024-42-3bros-original-imagz8zkvwjtayrs.jpeg?q=70"
                 alt="test"
               />
 
-              {cartArray.map((product)=>{
-                return <div className="info m-3 flex align-middle  justify-between" style={{alignItems:"center"}}>
-                <Badge
-                badgeContent={product?.quantity}
-                className="text-pink-500"
-                color="secondary"
-                style={{ color: "pink" }}
-              >
+              {cartArray.map((product) => {
+                return <div className="info m-3 flex align-middle  justify-between" style={{ alignItems: "center" }}>
+                  <div className="">
+
+                  <Badge
+                    badgeContent={product?.quantity}
+                    className="text-pink-500 p-0 z-10 relative ml-14 mt-5"
+                    color="secondary"
+                    style={{ color: "pink" }}
+                  >
+                  
+                  </Badge>
                   <img
-                    width={"50px"}
-                    height={"50px"}
-                    className="mr-4"
-                    src={product?.img}
-                  />
-              </Badge>
-                
+                      className="mr-4 h-[60px] w-[60px] rounded-full"
+                      src={product?.img}
+                    />
+                  </div>
+
+
                   <p className="font-bold text-2xl ml-1">
                     {product?.title}
                   </p>
-                  <div className="flex justify-between gap-4 ml-8" style={{width:"80px"}}>
+                  <div className="flex justify-between gap-4 ml-8" style={{ width: "80px" }}>
                     <button
-                      onClick={()=>decreaseQuantity(product?._id)}
+                      onClick={() => decreaseQuantity(product?._id)}
                       className="font-bold ml-1 cursor-pointer shadow-xl text-xl"
                     >
                       -
@@ -120,7 +123,7 @@ router.push("/")
                       {product?.quantity}
                     </button>
                     <button
-                      onClick={()=>increaseQuantity(product?._id)}
+                      onClick={() => increaseQuantity(product?._id)}
                       className="font-bold ml-1 cursor-pointer shadow-xl text-xl"
                     >
                       +
@@ -133,13 +136,13 @@ router.push("/")
                 <h3 className="font-bold ml-3">subtotal :{calculateTotal()}</h3>
               </div>
               <div>
-                {cartArray.length >0 
+                {cartArray.length > 0
                   ? <Link href={"/checkout"} onClick={() => setState({ ...state, ["right"]: false })} className="p-2 bg-pink-500 text-white font-bold hover:bg-pink-400 m-1">
                     Checkout
-                  </Link>: <button disabled={true} className="p-1 rounded  text-white font-bold hover:bg-slate-300 m-1">
+                  </Link> : <button disabled={true} className="p-1 rounded  text-white font-bold hover:bg-slate-300 m-1">
                     Checkout
                   </button>}
-                <Button disabled={cartArray.length ? false :true} onClick={handleClear} className="bg-pink-500 text-white font-bold hover:bg-black m-1">
+                <Button disabled={cartArray.length ? false : true} onClick={handleClear} className="bg-pink-500 text-white font-bold hover:bg-black m-1">
                   Clear
                 </Button>
                 <div>
